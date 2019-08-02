2bd51c99b7606f79895fb61def652c51de96b8ec
dzpa <dzpa_silent@gmail.com>
2019-08-02 18:33:13 +0200

    Add docker installation script for cuda9.1

---
+++ install_docker_nvidia.sh
@@ -0,0 +1,30 @@
+# 1. Install docker
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce
+docker -v
+sudo usermod -aG docker Username
+su - Username
+
+
+# 2.0 Install nvidia drivers
+
+# sudo apt-get remove nvidia -390 ; sudo apt-get install nvidia-390
+
+
+# 2.1 Install nvidia docker
+
+curl -s -L https://nvidia.github.io/nvidia-docker/gpgkey | \
+sudo apt-key add -
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID)
+curl -s -L https://nvidia.github.io/nvidia-docker/$distribution/nvidia-docker.list | \
+sudo tee /etc/apt/sources.list.d/nvidia-docker.list
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+sudo pkill -SIGHUP dockerd
+docker run --runtime=nvidia --rm nvidia/cuda:9.1-base nvidia-smi
