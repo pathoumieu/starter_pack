4468d85ae5f1d3211e227ceec2daa3a123ed04e2
dzpa <dzpa_silent@gmail.com>
2019-08-05 11:00:46 +0200

    Update virtualenvwrapper install

--- install_virtualwrapper.sh
+++ install_virtualwrapper.sh
@@ -4,8 +4,8 @@ sudo apt-get install python3-pip
 
 sudo pip3 install virtualenvwrapper
 
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/Devel
-export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3
-export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'
+echo "export WORKON_HOME=$HOME/.virtualenvs">>~/.bashrc
+echo "export PROJECT_HOME=$HOME/Devel">>~/.bashrc
+echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3">>~/.bashrc
+echo "export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'">>~/.bashrc
 source /usr/local/bin/virtualenvwrapper.sh
