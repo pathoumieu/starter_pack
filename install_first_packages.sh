69840e05be3ade671b8e1365f2884c30dbeaf950
dzpa <dzpa_silent@gmail.com>
2020-05-03 15:28:46 +0200

    Update install first_packages script

--- install_first_packages.sh
+++ install_first_packages.sh
@@ -5,6 +5,7 @@ sudo apt-get install vim
 sudo apt-get install terminator
 sudo apt install openssh-server
 sudo apt-get install net-tools
+sudo apt-get install gitg
 
 cp .bash_aliases ~/
 source ~/.bash_aliases
