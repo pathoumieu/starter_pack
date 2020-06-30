9e00b74afbdc7b6fbb7578ae6e2c148e7c42353f
dzpa <dzpasilent@gmail.com>
2020-06-30 15:52:25 +0200

    Update virtualenvwrapper installer

--- install_virtualwrapper.sh
+++ install_virtualwrapper.sh
@@ -7,5 +7,4 @@ set -e
 echo "export WORKON_HOME=$HOME/.virtualenvs">>~/.bashrc
 echo "export PROJECT_HOME=$HOME/Devel">>~/.bashrc
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3">>~/.bashrc
-echo "export VIRTUALENVWRAPPER_VIRTUALENV_ARGS='--no-site-packages'">>~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh">>~/.bashrc
