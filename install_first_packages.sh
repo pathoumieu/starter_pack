db792d91415f7a0db140497f03dd567a65f2d483
pathoumieu <37932858+pathoumieu@users.noreply.github.com>
2022-05-24 14:58:00 +0200

    Update install_first_packages.sh

--- install_first_packages.sh
+++ install_first_packages.sh
@@ -5,8 +5,16 @@ sudo apt-get install vim
 sudo apt-get install terminator
 sudo apt install openssh-server
 sudo apt-get install net-tools
-sudo apt-get install gitg
+sudo apt-get install git
+sudo apt-get install gitk
+
+sudo snap install slack
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+sudo snap install --classic code
 
 cp .bash_aliases ~/
 source ~/.bash_aliases
 cp .gitconfig ~/
+
+ssh-keygen -b 2048 -t rsa
